7f66eb2e3b70708f07f9ecd7996b1fe1f1267f7b
Ann-Anyango <annmwangombe@gmail.com>
2023-04-11 08:58:20 +0300

    first commit

---
+++ string.js
@@ -0,0 +1,57 @@
+// Write a function that takes in a string and returns it when reversed
+// let food = “eating”
+function food1(string){
+    console.log(food);
+}
+let food="eating"
+food1(food)
+// Write a function that takes in the following array and consoles the target if it is found else
+// null
+// let num = [2,8,0,23,5,45,76]
+// Target = 23
+function arr(arr,target){
+    left_start=0;
+    end=0;
+    if(left_start<arr){
+        return
+    }
+
+
+
+}
+let num =[2,8,0,23,5,45,76];
+// Given years between 2000 and 2023, console all the leap years in the following
+// sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
+let years=[2000,2023];
+leap(i=0,years.length,++i);
+if (years .length<=[i]){
+    return 
+
+}
+
+
+
+// Write a function that takes in an array of numbers and returns an array that has all
+// elements multiplied by 4.
+// let numArray = [12,87,45,75,23,64,73]
+// function Arr(num){
+//     num =0;
+//     if(Arr.length)
+function Arr(nam){
+number (nam)
+function number(nam){
+    nam.forEach(element => {
+        console.log(num*4)
+    });
+}
+}
+
+let numArray=[12,87,45,75,23,64,73];
+// Write a function that takes in an array of strings and returns an array with every element
+// turned into a number
+// let nums = [“10”,”24”,”45”,”56”,”67”]
+function not (arr){
+    let frin=
+    arr((num.length)/2);
+    num <=arr
+}
